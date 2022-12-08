f7405230ea9f1e20143ccc89905fee6fa1310536
Nicholas Ly <nichdly@gmail.com>
2022-12-08 15:22:20 -0500

    Day 8, part 2 solution

--- 2022/8/day8.py
+++ 2022/8/day8.py
@@ -8,11 +8,57 @@ class Tree:
     height: int
     visible: bool = False
 
-def markOutsideVisbleTrees(forest: list[list[Tree]]):
+def computeScenicScore(forest: list[list[Tree]], row: int, col: int) -> int:
     """
-    Marks all trees in a forest visible only if they are visible from outside the forest.
+    Computes the scenic score of a tree at a specified location in the forest.
     """
-    length = len(forest)  # Assumes forest is a square matrix
+    length = len(forest)  # NOTE: Assumes forest is a square matrix
+    distances = {
+        'right' : 0,
+        'down' : 0,
+        'left' : 0,
+        'up' : 0
+    }
+
+    # Mark all trees visible by looking right
+    vision = col + 1
+    while vision < length:
+        distances['right'] += 1
+        if forest[row][vision].height >= forest[row][col].height:
+            break
+        vision += 1
+
+    # Mark all trees visible by looking down
+    vision = row + 1
+    while vision < length:
+        distances['down'] += 1
+        if forest[vision][col].height >= forest[row][col].height:
+            break
+        vision += 1
+
+    # Mark all trees visible by looking left
+    vision = col - 1
+    while vision >= 0:
+        distances['left'] += 1
+        if forest[row][vision].height >= forest[row][col].height:
+            break
+        vision -= 1
+
+    # Mark all trees visible by looking up
+    vision = row - 1
+    while vision >= 0:
+        distances['up'] += 1
+        if forest[vision][col].height >= forest[row][col].height:
+            break
+        vision -= 1
+
+    return distances['right'] * distances['down'] * distances['left'] * distances['up']
+
+def partOne(forest: list[list[Tree]]):
+    """
+    Prints the number of visible trees from outside the forest.
+    """
+    length = len(forest)  # NOTE: Assumes forest is a square matrix
 
     # Mark all trees visible by looking down or right
     for a in range(length):
@@ -38,22 +84,32 @@ def markOutsideVisbleTrees(forest: list[list[Tree]]):
                 forest[b][a].visible = True
                 tallestUp = forest[b][a].height
 
-def partOne(forest: list[list[int]]):
-    """
-    Prints the number of visible trees from outside the forest.
-    """
-    markOutsideVisbleTrees(forest)
+    # Counts all visible trees
     counter = 0
     for row in forest:
         for tree in row:
             if tree.visible:
                 counter += 1
-    print(counter)
+    print( counter )
+
+def partTwo(forest: list[list[Tree]]):
+    """
+    Prints the highest 'scenic score' possible for any tree in the forest.
+    """
+    length = len(forest)  # NOTE: Assumes forest is a square matrix
+    highestScenicScore = 0
+    for row in range(length):
+        for col in range(length):
+            score = computeScenicScore(forest, row, col)
+            if score > highestScenicScore:
+                highestScenicScore = score
+    print( highestScenicScore )
 
 def main():
     with open("input.txt", "r") as file:
         forest = [ [ Tree(height=int(height)) for height in row.strip() ] for row in file.readlines() ]
         partOne(forest)
+        partTwo(forest)
 
 if __name__ == "__main__":
     main()
